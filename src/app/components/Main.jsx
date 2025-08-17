1d5f09cd385fa840060ca4f802cbeb3da5792e7d
nvrsunk <nvrsdark@gmail.com>
2025-08-17 15:46:01 +0200

    g

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -13,7 +13,7 @@ function Main() {
     <>
     <span><span className='text-white'>Midnights</span> Album Available! </span>
     <span>Compralo antes de que se acaben.</span>
-    <span>Buy your tickets for <span className='text-white'>#Tour2025</span> before June</span>
+    <span>Buy your tickets for <span className='text-white/80'>#Tour2025</span> before June</span>
     <span>10% discount in album store</span>
     </>
   ]
@@ -21,23 +21,23 @@ function Main() {
   return (
     <>
       <header className='flex flex-col w-full relative xl:mx-96'>
-          <section className='absolute z-10 top-10 flex flex-col text-center left-1/2 -translate-x-1/2'>
+          <section className='absolute z-10 top-10 flex flex-col text-center w-full left-1/2 -translate-x-1/2'>
             <h1 className=' text-[22px] text-white/60'>Taylor Alisson Swift</h1>
-            <p className='text-[14px] space-x-3 lg:text-sm text-white/40 mb-3'>
-              Country • ElectroPop • Pop
+            <p className='text-[12px] space-x-3 lg:text-sm text-white/40 mb-3'>
+              Country - ElectroPop - Pop
             </p>
            <span className='text-sm mb-3 text-white/50'>Album of the year nomination</span>
           </section>
       
         
         
-         <p className='absolute z-10 flex flex-col items-end bottom-10 right-8 text-end lg:bottom-8 text-white/50'>
+         <p className='absolute z-10 flex flex-col items-end bottom-10 right-6 text-end lg:bottom-8 text-white/50'>
           <span className={`text-start text-[18px] lg:text-[18px] `}>#1 USA on Spotify</span>
           <span className='secondTop'>#1 Canada Spotify</span>
           <span className='secondTop'>#2 Apple Music</span>
-          <button className='mt-4 flex items-center gap-2 text-xs bg-white/10 rounded-md py-3 pl-4 pr-3 w-fit text-white'>
+          <button className='mt-4 flex items-center gap-2 text-sm bg-white/10 rounded-md py-3 pl-4 pr-3 w-fit text-white'>
             <Link href="/albums">{textos.stream}</Link>
-            <Image src='/arrowLink.svg' alt="" height={11} width={11}  />
+            <Image src='/arrowLink.svg' alt="" height={11} width={12}  />
           </button>
         </p> 
         <section className='h-[36rem]'>
@@ -49,7 +49,7 @@ function Main() {
         <section className='relative overflow-hidden w-full'>
           <div className=" w-max carousel-track bg-black/60 space-x-14">
           {Array(4).fill(mensajes[0]).map((mensaje,index) => (
-           <p key={index} className="shrink-0 space-x-14 py-[10px] px-4 text-[12px] text-white/70 inter-200 flex lg:text-sm ">
+           <p key={index} className="shrink-0 space-x-14 py-3 px-4 text-[14px] text-white/60 inter-200 flex lg:text-sm ">
             {mensaje}
           </p>
           ))}
